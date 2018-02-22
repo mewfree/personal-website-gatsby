56d62d04b7c4afede7a8785da2dc7bb93414dbfa
Damien Gonot <damien.gonot@gmail.com>
2018-02-21 23:15:30 -0500

    homepage copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,13 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1 className="title">
-      Hi everybody
+      Introduction
     </h1>
     <p>
-      My name is Damien Gonot.
+      All-in-one marketer & developer. Currently a User Acquisition Manager at Hopper, previously at Busbud.
+    </p>
+    <p>
+      Proudly living in Montréal ❤️
     </p>
     <Link to="/page-2/">
       Go to page 2
