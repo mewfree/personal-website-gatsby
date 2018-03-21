7a33cb02eb9fcdcaf8d6ce9ddb950a0d7cf63d32
Damien Gonot <damien.gonot@gmail.com>
2018-03-21 19:14:01 -0400

    better high def image support

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,7 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 800,
+              sizeByPixelDensity: true,
             },
           },
         ],
