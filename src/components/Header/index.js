2fc0df309d08e8fdad493235c6a0619b612b2011
Damien Gonot <damien.gonot@gmail.com>
2019-07-14 12:55:08 -0400

    wrap h1 title by link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,11 +5,11 @@ const Header = () => (
   <section className="hero is-primary">
     <div className="hero-body">
       <div className="container has-text-centered">
-        <h1 className="title">
-          <Link to="/">
-            Damien Gonot
-          </Link>
-        </h1>
+        <Link to="/">
+          <h1 className="title header-title">
+              Damien Gonot
+          </h1>
+        </Link>
         <h2 className="subtitle">
           Personal blog
         </h2>
